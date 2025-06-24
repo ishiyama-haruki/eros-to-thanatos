65702067597d1877dce31b79a0b788d66e38b665
ishiyamaharuki <haruki0311ishiyama@gmail.com>
2025-06-24 21:37:20 +0900

    初期設定

---
+++ src/Upload.jsx
@@ -0,0 +1,12 @@
+function Upload() {
+    return (
+        <div>
+            <button type="button">クリック</button>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    )
+}
+
+export default Upload;
